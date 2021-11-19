4fb2b62e8a1812a0db25989ccc1b9e33f948b12a
wxyforgithub <wxyforgithub@163.com>
2021-11-19 15:44:29 +0800

    增加国际化

--- src/main.js
+++ src/main.js
@@ -5,11 +5,13 @@ import router from '@/router/router';
 import './common/common.scss';
 import { Toast } from 'vant';
 import 'vant/lib/toast/style';
+import i18n from './il8n';
 Vue.use(Toast);
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  i18n,
   render: h => h(App),
 }).$mount('#app')
